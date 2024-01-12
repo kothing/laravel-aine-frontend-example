565beef02b8989b287f58cf337d30d6341a1bb86
Lyove <lyove@outlook.com>
2024-01-12 14:18:58 +0800

    init

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -92,7 +92,7 @@
 
         <!-- Sidebar Section -->
         <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
-            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+            <div class="w-full shadow flex flex-col my-4 p-6 rounded-md">
                 <p class="text-xl font-semibold pb-5">About Us</p>
                 <p class="pb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mattis est eu odio sagittis tristique. Vestibulum ut finibus leo. In hac habitasse platea dictumst.</p>
                 <a href="/page/about" class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
@@ -100,8 +100,7 @@
                 </a>
             </div>
             
-            
-            <div class="w-full bg-white shadow my-4 p-6">
+            <div class="w-full shadow my-4 p-6 rounded-md">
                 <p class="text-xl font-semibold pb-5">Tags</p>
                 @foreach($tags as $ta)
                     <a href="/tag/{{ $ta['tag'] }}" class="mr-2 mb-2 inline-block px-3 py-1 bg-gray-100 rounded-md hover:bg-gray-200">{{ $ta['tag'] }}</a>
